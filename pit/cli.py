ecd37adf7e189465a8c7bc6b7d55ca2b5ed3ada7
Marshall Ward <marshall.ward@gmail.com>
2014-09-18 09:58:44 +1000

    Minimal command line support

--- pit/cli.py
+++ pit/cli.py
@@ -1,3 +1,33 @@
+"""pit.cli
+   =======
+
+   Command line interpreter module for pit
+
+   :copyright: Copyright 2014 Aidan Heerdegen and Marshall Ward
+   :license: Apache License, Version 2.0, see LICENSE for details
+"""
+
+# Standard Library
+import argparse
+import sys
+
+# Local
+from pit import __version__
+
 def parse():
-    print('NYI')
-    sys.exit()
+    """Parse command line inputs and execute the subcommand."""
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--version', action='version',
+                        version='pit {}'.format(__version__))
+
+    # TODO: Organise subcommands
+    subparsers = parser.add_subparsers()
+
+    # Display help if no arguments are provided
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        print(args)
